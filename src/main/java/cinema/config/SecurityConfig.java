97545bd0edc8177100404c51e838aba58ce9e255
dinhhoa04 <22t1020123@husc.edu.vn>
2026-08-01 00:57:46 +0700

    Add Dockerfile for Render

--- src/main/java/cinema/config/SecurityConfig.java
+++ src/main/java/cinema/config/SecurityConfig.java
@@ -91,7 +91,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:5174", "http://localhost:3000")); // Link Frontend của anh
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:5174", "http://localhost:3000", "http://157.66.218.164",
+                "http://cinemabookcdh.duckdns.org")); // Link Frontend của anh
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
