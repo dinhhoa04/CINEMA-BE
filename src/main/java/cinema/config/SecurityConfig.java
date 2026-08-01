a50f7768cd6212b657961f88b2ee496ad167b6b0
dinhhoa04 <22t1020123@husc.edu.vn>
2026-08-01 13:58:37 +0700

    Allow Vercel CORS origin

--- src/main/java/cinema/config/SecurityConfig.java
+++ src/main/java/cinema/config/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:5174", "http://localhost:3000", "http://157.66.218.164",
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:5174", "http://localhost:3000", "http://157.66.218.164", "https://cinema-fe-rho.vercel.app",
                 "http://cinemabookcdh.duckdns.org")); // Link Frontend của anh
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
